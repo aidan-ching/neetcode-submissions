841a5adaf43465e07aa3932554ad7a4be1edef1f
Aidan Ching <75757836+aidan-ching@users.noreply.github.com>
2026-09-13 15:47:49 -0700

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = [0]*26
+        t_count = [0]*26
+        for c in s:
+            s_count[ord(c)-ord('a')] += 1
+        for c in t:
+            t_count[ord(c)-ord('a')] += 1
+
+        return tuple(s_count) == tuple(t_count)
